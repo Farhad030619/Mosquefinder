0c6f993bc2dac632b32ebe5b3fd6acb37a5bc0a3
Farhad164 <farhad.jelve.03@gmail.com>
2025-06-11 02:45:48 +0200

    Tog bort gamla listan av moskeer och gjorde en javascript fil som kan ta nya moskeer från en json fil istället, där kan vi uppdatera nya moskeer enklare

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,52 @@
-import { MAP_API_KEY } from '../config.js';
-window.initMap = (lat = 59.3293, lng = 18.0686) => {
-  const map = new google.maps.Map(document.getElementById('map'), { center: { lat, lng }, zoom: 12 });
-  new google.maps.Marker({ position: { lat, lng }, map });
+window.initMap = function () {
+  const mapDiv = document.getElementById("map");
+  if (!mapDiv) return;
+  const defaultCenter = { lat: 59.3293, lng: 18.0686 }; // Stockholm
+  const map = new google.maps.Map(mapDiv, {
+      zoom: 11,
+      center: defaultCenter,
+      mapTypeId: "roadmap"
+  });
+
+  fetch('/moskeer.json')
+      .then(res => {
+          if (!res.ok) throw new Error("Kunde inte ladda moskeer.json!");
+          return res.json();
+      })
+      .then(mosques => {
+          mosques.forEach(mosque => {
+              if (typeof mosque.lat === "number" && typeof mosque.lng === "number") {
+                  const marker = new google.maps.Marker({
+                      position: { lat: mosque.lat, lng: mosque.lng },
+                      map,
+                      title: mosque.namn,
+                      icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                  });
+                  const infoWindow = new google.maps.InfoWindow({
+                      content: `<b>${mosque.namn}</b><br>${mosque.entrance || ""}<br>${mosque.beskrivning || ""}`
+                  });
+                  marker.addListener('click', () => infoWindow.open(map, marker));
+              }
+          });
+      })
+      .catch(err => {
+          console.error("Fel vid laddning av moskeer.json:", err);
+      });
+
+  // Visa användarens plats
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+          const userLoc = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+          };
+          new google.maps.Marker({
+              position: userLoc,
+              map: map,
+              title: "Du är här",
+              icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+          });
+          map.setCenter(userLoc);
+      });
+  }
 };
-const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${MAP_API_KEY}&callback=initMap`;
-script.defer = true;
-document.head.appendChild(script);
